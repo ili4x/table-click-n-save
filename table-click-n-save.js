ec1d4d66b951469178d6f55b08308def2894e927
ili4 <ili4@ili4-PC>
2019-06-05 22:32:51 +0300

    jQuery load

--- table-click-n-save.js
+++ table-click-n-save.js
@@ -1,8 +1,22 @@
 var tableCLickNSave_base = 'https://ili4x.github.io/table-click-n-save';
 
-$.getScript(tableCLickNSave_base + '/unpkg.com/FileSaver.min.js');
-$.getScript(tableCLickNSave_base + '/unpkg.com/xlsx.min.js');
-$('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', tableCLickNSave_base + '/table-click-n-save.css'));
+
+function tableCLickNSave_loadcss(src) {
+	var link = document.createElement('link');
+	link.rel = "stylesheet";
+	link.type = "text/css"
+	link.href = src;
+	document.head.appendChild(link);
+}
+
+function tableCLickNSave_loadscript(src, onload) {
+	var script = document.createElement('script');
+	script.src = src;
+	if (onload) {
+		script.onload = onload;
+	}
+	document.head.appendChild(script);
+}
 
 function tableCLickNSave_mouseenter(ev) {
 	$(ev.currentTarget).addClass('table-click-n-save-selected');
@@ -11,7 +25,7 @@ function tableCLickNSave_mouseleave(ev) {
 	$(ev.currentTarget).removeClass('table-click-n-save-selected');
 }
 
-function tableCLickNSave_init() {
+function tableCLickNSave_start() {
 	$('table').on('mouseenter', tableCLickNSave_mouseenter).on('mouseleave', tableCLickNSave_mouseleave).on('click', tableCLickNSave_saveTable);
 }
 
@@ -23,10 +37,17 @@ function tableCLickNSave_end() {
 function tableCLickNSave_saveTable(ev) {
 	var table = ev.currentTarget;
 	var type = 'xls';
-	var wb = XLSX.utils.table_to_book(table, {sheet: "filename"});
-	XLSX.writeFile(wb, "filename" + '.' + (type || 'xlsx'));
+	var fn = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
+	var wb = XLSX.utils.table_to_book(table, {sheet: fn});
+	XLSX.writeFile(wb, fn + '.' + (type || 'xlsx'));
 	tableCLickNSave_end();
 }
 
-
-tableCLickNSave_init();
+//tableCLickNSave_loadscript(tableCLickNSave_base + '/unpkg.com/FileSaver.min.js');
+tableCLickNSave_loadscript(tableCLickNSave_base + '/unpkg.com/xlsx.min.js');
+tableCLickNSave_loadcss(tableCLickNSave_base + '/table-click-n-save.css');
+if (typeof jQuery === 'undefined') {
+	tableCLickNSave_loadscript("https://code.jquery.com/jquery-3.4.1.min.js", tableCLickNSave_start);
+} else {
+	tableCLickNSave_start();
+}
